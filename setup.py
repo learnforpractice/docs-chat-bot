c7ffabda259e2f3c8a1c2bbad00498bca7fca63b
learnforpractice <learnforpractice@gmail.com>
2023-04-11 10:20:50 +0800

    Bump version to v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="docs-chat-bot",
-    version="0.0.1",
+    version="0.0.2",
     description="docs chat bot by leveraging CHATGPT",
     long_description=long_description,
     long_description_content_type="text/markdown",
